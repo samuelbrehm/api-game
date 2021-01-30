ea2c7665339fb0baa7b19545fb437ad5d0f5d161
Samuel <samuelbrehm@gmail.com>
2021-01-29 21:51:16 -0300

    Fix config plugin aws-s3 for files uploads

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3',
+    provider: "aws-s3",
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: 'AWS_REGION',
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
       params: {
-        Bucket: 'AWS_BUCKET',
+        Bucket: env("AWS_BUCKET"),
       },
     },
   },
