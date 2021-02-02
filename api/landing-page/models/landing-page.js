f5d84174982bf6f706e6e689ba0f0dc473397028
Samuel <samuelbrehm@gmail.com>
2021-02-02 07:40:46 -0300

    Setup Netlify WebHook

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -5,4 +5,17 @@
  * to customize this model
  */
 
-module.exports = {};
+const axios = require("axios");
+const netlifyWebhook = strapi.config.get("custom.netlifyWebhook")
+
+module.exports = {
+  lifecycles: {
+    async afterCreate(_, __) {
+      netlifyWebhook && axios.post("netlifyWebhook")
+    },
+
+    async afterUpdate(_, __, ___) {
+      netlifyWebhook && axios.post("netlifyWebhook")
+    }
+  },
+};
