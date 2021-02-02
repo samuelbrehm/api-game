a28fde3ef5b0663203a7449ed2c51134d7e552d7
Samuel <samuelbrehm@gmail.com>
2021-02-02 07:54:45 -0300

    Fix config webhook netlify

--- api/landing-page/models/landing-page.js
+++ api/landing-page/models/landing-page.js
@@ -6,16 +6,18 @@
  */
 
 const axios = require("axios");
-const netlifyWebhook = strapi.config.get("custom.netlifyWebhook")
+const netlifyWebhook = strapi.config.get("custom.netlifyWebhook");
 
 module.exports = {
   lifecycles: {
     async afterCreate(_, __) {
-      netlifyWebhook && axios.post("netlifyWebhook")
+      netlifyWebhook && axios.post(netlifyWebhook);
     },
 
     async afterUpdate(_, __, ___) {
-      netlifyWebhook && axios.post("netlifyWebhook")
-    }
+      netlifyWebhook && axios.post(netlifyWebhook);
+    },
   },
 };
+
+
