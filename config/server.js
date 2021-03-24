093620247cb11495aedfc6a792c63e69bb3ce601
Samuel <samuelbrehm@gmail.com>
2021-03-24 12:32:30 -0300

    update configs

--- config/server.js
+++ config/server.js
@@ -1,9 +1,17 @@
+const forgotPasswordTemplate = require('./email-templates/forgot-password');
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fb8fa499386d0d194cb3858343c2c012'),
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+
+    forgotPassword: {
+      from: 'support@mywebsite.fr',
+      replyTo: 'support@mywebsite.fr',
+      emailTemplate: forgotPasswordTemplate,
     },
   },
 });
